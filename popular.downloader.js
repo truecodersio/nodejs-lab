b389471eaaeb6d47a3941e07d71aeb73dc3e4c78
Ben Bryant <brbryant2639@gmail.com>
2020-03-21 15:55:38 -0500

    deleted test console.log statements

--- popular.downloader.js
+++ popular.downloader.js
@@ -2,13 +2,10 @@ const path = require("path");
 const fs = require("fs");
 const rp = require("request-promise");
 
-// const dataPath = path.join(__dirname, "/downloads");
-
 rp("https://reddit.com/r/popular.json", (err, res, body) => {
   if (err) console.log(err);
 
   JSON.parse(body).data.children.forEach(article => {
-    //   console.log(article.data.url);
     if (
       path.extname(article.data.url) === ".png" ||
       path.extname(article.data.url) === ".jpg" ||
